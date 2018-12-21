37c09bc0980a062d19d7c5fb81261f6ee849c1d9
Larry Shaffer <lshaffer@boundlessgeo.com>
2018-12-21 16:47:34 -0700

    Clean up bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,14 @@
 
 cd win32
 
-:: Build using ming64 via msys2's bash
+:: Build using ming64 via msys2
 :: mingw32-make.exe
 make.exe -f mingwin32.mak clean
 if %errorlevel% neq 0 exit /b %errorlevel%
 make.exe -f mingwin32.mak WIDE=Y UTF8=Y
 if %errorlevel% neq 0 exit /b %errorlevel%
 make.exe -f mingwin32.mak WIDE=Y UTF8=Y DLL=Y libs
-REM if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 if not exist %LIBRARY_LIB% mkdir %LIBRARY_LIB%
 if not exist %LIBRARY_BIN% mkdir %LIBRARY_BIN%
